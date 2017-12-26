4ee3c92805da4cd755603981967d48d08c471280
Mondmaxy <mondmaxy@gmail.com>
2017-12-26 15:15:23 -0300

    echo

--- 000-default.conf.sh
+++ 000-default.conf.sh
@@ -1,6 +1,10 @@
+APP_LOCATION=$1
+
+cat  << EOF
 <VirtualHost *:80>
     ServerAdmin admin@pruebas.com
-    DocumentRoot /home/deploy/app/${APP_LOCATION}
+    DocumentRoot /home/deploy/app/$APP_LOCATION
     ErrorLog ${APACHE_LOG_DIR}/error.log
     CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
+EOF
--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ echo ""
 echo "Using new Virtual Host file"
 echo ""
 echo "App Location: $APP_LOCATION"
-cp /home/deploy/script/000-default.conf /etc/apache2/sites-available/000-default.conf
+sh /home/deploy/script/000-default.conf.sh $APP_LOCATION > /etc/apache2/sites-available/000-default.conf
 
 
 echo $(cat /etc/apache2/sites-available/000-default.conf)
