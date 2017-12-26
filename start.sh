a89f6ecd42384505390e3aa62b7c99680ab224d8
Mondmaxy <mondmaxy@gmail.com>
2017-12-26 16:18:35 -0300

    index img

--- start.sh
+++ start.sh
@@ -10,13 +10,13 @@ cp /home/deploy/script/apache2.conf /etc/apache2/apache2.conf
 
 
 echo ""
-echo "Using new Virtual Host file"
+echo "Setting VirtualHost configuration"
+echo "DocumentRoot: $APP_LOCATION"
 echo ""
-echo "App Location: $APP_LOCATION"
-echo "DIRECTORY_INDEX:" $DIRECTORY_INDEX
 sh /home/deploy/script/000-default.conf.sh $APP_LOCATION > /etc/apache2/sites-available/000-default.conf
 
+echo ""
+echo "Setting Root File"
+echo "Root Index: $DIRECTORY_INDEX"
+echo ""
 sh /home/deploy/dir.conf.sh $DIRECTORY_INDEX > /etc/apache2/mods-enabled/dir.conf
-
-echo $(cat /etc/apache2/sites-available/000-default.conf)
-echo $(cat /etc/apache2/mods-enabled/dir.conf)
