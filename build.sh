f581d0b2f0ed5f64abecf4917c2999e19e48389f
Mondmaxy <mondmaxy@gmail.com>
2017-12-26 12:18:02 -0300

    index.html

--- build.sh
+++ build.sh
@@ -10,3 +10,7 @@ apt-get -y install apache2
 
 #Define servername to avoid warning msg
 echo "ServerName localhost" >> /etc/apache2/apache2.conf
+
+cp index.html /var/www/html/index.html
+
+systemctl restart apache2
