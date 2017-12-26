ed58d19c9aad160eb24e1a7fdc011f50cc488094
Mondmaxy <mondmaxy@gmail.com>
2017-12-26 11:11:59 -0300

    first commit

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Auto detected script"
+
+#Update
+apt-get -y update
+
+#Install apache
+apt-get -y install apache2
+
+#Define servername to avoid warning msg
+echo "ServerName localhost" >> /etc/apache2/apache2.conf
