6e7dd789348fbfdc2fd63bafb8e9bcb957cc5634
Mondmaxy <mondmaxy@gmail.com>
2017-12-26 13:11:16 -0300

    apache conf

--- start.sh
+++ start.sh
@@ -2,4 +2,14 @@
 
 echo "start"
 
+#Setting permissions
+echo ""
+echo "Setting Permissions"
+echo ""
+cp /home/deploy/script/apache2.conf /etc/apache2/apache2.conf
+
+
+echo ""
+echo "Using new Virtual Host file"
+echo ""
 cp /home/deploy/script/000-default.conf /etc/apache2/sites-available/000-default.conf
